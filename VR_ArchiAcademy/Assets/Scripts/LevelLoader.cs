6147e49fb51ee45b18bcee6c06b01081a433f0cd
elaine2700 <easerrano.e@gmail.com>
2022-06-20 21:01:08 -0700

    Added locomotion, Start Scene
    Start scene loads game Scene.
    Added locomotion system.

---
+++ VR_ArchiAcademy/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public void GoToScene(string nextScene)
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+}
