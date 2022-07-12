3a52e488256353ce7d43875bb462e70648bfdbfe
elaine2700 <easerrano.e@gmail.com>
2022-07-11 21:29:39 -0700

    Skybox and new shader
    New shader - Render Style: Sketch
    Hand painted skybox on first scene
    Added terrain and baked light

---
+++ VR_ArchiAcademy/Assets/Shaders/GettingLight.hlsl
@@ -0,0 +1,9 @@
+﻿void MainLight_half(out half3 Direction)
+{
+    #ifdef SHADERGRAPH_PREVIEW
+        Direction = half3(0,1,0);
+    #else
+        Light light = GetMainLight();
+        Direction = light.direction;
+    #endif
+}
